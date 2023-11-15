ce106f33255fccae49e59301a687a183668ca670
Navanath Bhosale <navanathb@bsf.io>
2023-11-15 10:40:27 +0530

    fix: Astra Integrations string updated with white label

--- admin/assets/build/dashboard-app.asset.php
+++ admin/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '89f41e23a32f3e82b08c');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4f1af2692e8c7a78bb54');
