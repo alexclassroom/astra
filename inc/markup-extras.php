d282b27a719f6f4ca64fb8e57fd09c8f4c8a0669
Navanath Bhosale <navanathb@bsf.io>
2023-12-01 17:32:41 +0530

    fix: featured image size not working for banner layout 1

--- inc/markup-extras.php
+++ inc/markup-extras.php
@@ -1801,11 +1801,12 @@ if ( ! function_exists( 'astra_get_post_thumbnail' ) ) {
 
 			if ( $featured_image && ( ! ( $check_is_singular ) || ( ! post_password_required() && ! is_attachment() && has_post_thumbnail() ) ) ) {
 
+				$image_size = $check_is_singular ? astra_get_option( 'ast-dynamic-single-' . $post_type . '-article-featured-image-size', 'large' ) : 'large';
 				$post_thumb = apply_filters(
 					'astra_featured_image_markup',
 					get_the_post_thumbnail(
 						get_the_ID(),
-						apply_filters( 'astra_post_thumbnail_default_size', 'large' ),
+						apply_filters( 'astra_post_thumbnail_default_size', $image_size ),
 						apply_filters( 'astra_post_thumbnail_itemprop', '' )
 					)
 				);
