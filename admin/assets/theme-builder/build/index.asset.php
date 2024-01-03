79859f1b89c2c4c32e996e9b31941ff92659472e
Navanath Bhosale <navanathb@bsf.io>
2024-01-03 16:59:17 +0530

    fix: qa fixes

--- admin/assets/theme-builder/build/index.asset.php
+++ admin/assets/theme-builder/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '6e145d725ecdc261785e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'd2de54ff3e1c4112bc76');
